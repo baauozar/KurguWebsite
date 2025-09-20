9b7963ea006df1f6c6591e3e4fe9497f33694b27
haithm ba auozar <haithm.auozar@ogr.sakarya.edu.tr>
2025-09-20 09:33:48 +0300

    .

---
+++ src/Core/KurguWebsite.Application/Validators/Auth/LoginRequestValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using KurguWebsite.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurguWebsite.Application.Validators.Auth
+{
+    public class LoginRequestValidator : AbstractValidator<LoginRequest>
+    {
+        public LoginRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required");
+        }
+    }
+}
---
+++ src/Core/KurguWebsite.Application/Validators/Auth/ResetPasswordRequestValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using KurguWebsite.Application.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KurguWebsite.Application.Validators.Auth
+{
+    public class ResetPasswordRequestValidator : AbstractValidator<ResetPasswordRequest>
+    {
+        public ResetPasswordRequestValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required")
+                .EmailAddress().WithMessage("Invalid email format");
+
+            RuleFor(x => x.Token)
+                .NotEmpty().WithMessage("Reset token is required");
+
+            RuleFor(x => x.NewPassword)
+                .NotEmpty().WithMessage("New password is required")
+                .MinimumLength(6).WithMessage("Password must be at least 6 characters")
+                .MaximumLength(100).WithMessage("Password is too long")
+                .Matches(@"[A-Z]").WithMessage("Password must contain at least one uppercase letter")
+                .Matches(@"[a-z]").WithMessage("Password must contain at least one lowercase letter")
+                .Matches(@"[0-9]").WithMessage("Password must contain at least one number")
+                .Matches(@"[^a-zA-Z0-9]").WithMessage("Password must contain at least one special character");
+
+            RuleFor(x => x.ConfirmPassword)
+                .Equal(x => x.NewPassword).WithMessage("Passwords do not match");
+        }
+    }
+}
